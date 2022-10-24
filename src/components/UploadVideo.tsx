03563783fbd73f32a7b7762d890cafb38aa8b8ad
By-hoon <sbaru1997@naver.com>
2022-10-24 23:07:51 +0900

    [#7]feat: 비디오 미리보기 구현

--- src/components/UploadVideo.tsx
+++ src/components/UploadVideo.tsx
@@ -6,12 +6,19 @@ interface uploadVideoProps {
 }
 
 const UploadVideo = ({ videoFile, setVideoFile }: uploadVideoProps) => {
+  const [videoUrl, setVideoUrl] = useState("");
   const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const encodeFile = (fileBlob: Blob) => {
+    setVideoUrl(URL.createObjectURL(fileBlob));
+  };
   const onChangeVideo = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) {
       return;
     }
-    setVideoFile(e.target.files[0]);
+    const fileBlob = e.target.files[0];
+    setVideoFile(fileBlob);
+    encodeFile(fileBlob);
   }, []);
   const onUploadImageButtonClick = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -22,14 +29,19 @@ const UploadVideo = ({ videoFile, setVideoFile }: uploadVideoProps) => {
   }, []);
   return (
     <div className="video-upload__container">
-      <input
-        type="file"
-        className="display-none"
-        ref={inputRef}
-        accept="video/mp4,video/mkv, video/x-m4v,video/*"
-        onChange={onChangeVideo}
-      />
-      <button onClick={onUploadImageButtonClick}>비디오 선택</button>
+      <div className="video-input__container">
+        <input
+          type="file"
+          className="display-none"
+          ref={inputRef}
+          accept="video/mp4,video/mkv, video/x-m4v,video/*"
+          onChange={onChangeVideo}
+        />
+        <button onClick={onUploadImageButtonClick}>비디오 선택</button>
+      </div>
+      <div className="video-preview__container">
+        <div className="video-preview__container">{videoUrl ? <video src={videoUrl} controls /> : null}</div>
+      </div>
     </div>
   );
 };
