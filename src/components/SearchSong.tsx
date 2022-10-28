2cc14a13c5fde77223a57466a4dc7d6a1db9bc54
Yeunhun Bae <sbaru1997@naver.com>
2022-10-28 17:16:54 +0900

    [#8]feat: 호출한 노래 정보를 가공하여 songs에 저장

--- src/components/SearchSong.tsx
+++ src/components/SearchSong.tsx
@@ -2,19 +2,37 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { maniadbBaseUrl, xmlToJson } from "../shared/Constants";
 
+interface SongProps {
+  id: string;
+  title: string;
+  artist: string;
+  albumImage: string;
+}
+
 const SearchSong = () => {
   const [keyword, setKeyword] = useState("");
-  const [songs, setSongs] = useState([]);
+  const [songs, setSongs] = useState<Array<SongProps>>([]);
 
   const getSongs = async () => {
     const reqURL = maniadbBaseUrl(keyword, "song");
     await axios(reqURL).then((response) => {
       const dataSet = response.data;
       let XmlNode = new DOMParser().parseFromString(dataSet, "text/xml");
-      console.log(xmlToJson(XmlNode));
+      filterResponse(xmlToJson(XmlNode).rss.channel.item);
     });
   };
-
+  const filterResponse = (response: Array<any>) => {
+    const newResponse: Array<SongProps> = [];
+    response.forEach((element) => {
+      newResponse.push({
+        id: element["@attributes"].id,
+        title: element.title["#cdata-section"],
+        artist: element["maniadb:artist"].name["#cdata-section"],
+        albumImage: element["maniadb:album"].image["#cdata-section"],
+      });
+    });
+    setSongs(newResponse);
+  };
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     getSongs();
@@ -23,7 +41,6 @@ const SearchSong = () => {
   const onChangeKeyword = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
   }, []);
-
   return (
     <div className="search-song__container">
       <form onSubmit={onSubmit}>
@@ -35,6 +52,7 @@ const SearchSong = () => {
           onChange={onChangeKeyword}
         />
       </form>
+      {<>{songs.length ? console.log(songs) : null}</>}
     </div>
   );
 };
