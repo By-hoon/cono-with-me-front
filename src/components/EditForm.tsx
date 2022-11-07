4698c6c38cc6ed804e24498ed68eacd4d19528bc
Yeunhun Bae <sbaru1997@naver.com>
2022-11-08 02:24:08 +0900

    [#11]feat: 제목, 내용 수정 구현

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -1,13 +1,37 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { LiveData } from "../test/data";
 
 export const EditLiveForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const titleFocus = useRef<HTMLInputElement>(null);
+  const contentFocus = useRef<HTMLTextAreaElement>(null);
+
   const navigate = useNavigate();
+
+  const changeTitle = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }, []);
+  const changeContent = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  }, []);
+
   const goPreviousPage = () => {
     navigate(-1);
   };
   const editLive = () => {
     // 수정 api 호출
+    if (!content) contentFocus.current?.focus();
+    if (!title) titleFocus.current?.focus();
   };
+
+  useEffect(() => {
+    setTitle(LiveData.title);
+    setContent(LiveData.content);
+  }, []);
+
   return (
     <div className="edit__container">
       <div className="edit-upload-video__container">
@@ -26,7 +50,33 @@ export const EditLiveForm = () => {
         <div className="edit-header">
           <span className="edit__span">내용 수정</span>
         </div>
-        {/* 내용 수정 요소 */}
+        <div className="live-content-input__container">
+          <div className="title-input__container">
+            <div className="flex">
+              <div className="input-title__container">제목</div>
+              <input
+                type="text"
+                value={title}
+                placeholder="제목을 입력해 주세요."
+                onChange={changeTitle}
+                ref={titleFocus}
+                required
+              />
+            </div>
+          </div>
+          <div className="content-input__container">
+            <div className="flex">
+              <div className="input-title__container">내용</div>
+              <textarea
+                placeholder="내용을 입력해 주세요."
+                value={content}
+                onChange={changeContent}
+                ref={contentFocus}
+                required
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="button__container">
         <button className="cancle__button" onClick={goPreviousPage}>
