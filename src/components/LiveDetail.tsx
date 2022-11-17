1e4aea06c20d75c77566486d7997d15558f4c704
Yeunhun Bae <sbaru1997@naver.com>
2022-11-17 23:04:30 +0900

    [#13]feat: 비하인드 정보 구현
    - more option에서 설명 버튼 클릭 시 렌더링
    - 추후 필요에 따라 컴포넌트화

--- src/components/LiveDetail.tsx
+++ src/components/LiveDetail.tsx
@@ -1,8 +1,32 @@
+import { Icon } from "@iconify/react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { LiveProps } from "../shared/Props";
 
 const LiveDetail = () => {
   const { id, author, video, song, title, content } = useLocation().state as LiveProps;
+
+  const [showBehindInfo, setShowBehindInfo] = useState(false);
+  const [showMoreOption, setShowMoreOption] = useState(false);
+
+  const appearMoreOption = () => {
+    setShowMoreOption(true);
+  };
+  const disAppearMoreOption = () => {
+    setShowMoreOption(false);
+  };
+
+  const appearBehindInfo = () => {
+    setShowBehindInfo(true);
+  };
+  const disappearBehindInfo = () => {
+    setShowBehindInfo(false);
+  };
+
+  const clickShadow = () => {
+    if (showMoreOption) disAppearMoreOption();
+    if (showBehindInfo) disappearBehindInfo();
+  };
 
   return (
     <div className="live-detail__container">
@@ -13,13 +37,37 @@ const LiveDetail = () => {
           <div className="live-author">{author}</div>
         </div>
       </div>
-      <div className="tools__container">
-        <div className="live-content__container">{/* 클릭 시 라이브 비하인드 정보 컴포넌트 */}</div>
-        <div className="live-song__container">
+      <div className={`${showBehindInfo ? "live-behind-info__container" : "display-none"}`}>
+        <div className="flex">
+          <div className="live-behind-title">설명</div>
+          <div className="cancel-icon__container" onClick={disappearBehindInfo}>
+            <Icon icon="material-symbols:close" />
+          </div>
+        </div>
+        <div className="live-title--behind">{title}</div>
+        <div className="live-more-info__container">
+          {/* 좋아요, 조회수, 게시 날짜 등 부가적인 주요 정보 추가 */}
+        </div>
+        <div className="live-content">{content}</div>
+      </div>
+      <div className="live-tools__container flex">
+        <div className="live-tool__container" onClick={appearMoreOption}>
+          <Icon icon="material-symbols:more-horiz" />
+        </div>
+        <div className="live-tool__container">
           <img src={song.albumImage} alt="live-song" />
           {/* 클릭 시 자세한 노래 정보 컴포넌트 */}
         </div>
       </div>
+      <div className={`${showMoreOption ? "live-more-options__container flex" : "display-none"}`}>
+        <div className="live-more-option" onClick={appearBehindInfo}>
+          설명
+        </div>
+      </div>
+      <div
+        className={`${showBehindInfo || showMoreOption ? "shadow" : "display-none"}`}
+        onClick={clickShadow}
+      />
     </div>
   );
 };
