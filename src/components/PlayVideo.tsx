1e7613e8f9fee4e07b6bcfadbf1b519066d62981
Yeunhun Bae <sbaru1997@naver.com>
2022-11-18 22:10:48 +0900

    [#13]feat: react-player 적용
    - 클릭 시 음소거 on/off 기능 추가

--- src/components/PlayVideo.tsx
+++ src/components/PlayVideo.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+import ReactPlayer from "react-player";
+
 interface VideoProps {
   video: { [key: string]: string };
 }
 
 const PlayVideo = ({ video }: VideoProps) => {
+  const [isMuted, setIsMuted] = useState(true);
+
+  const controlMute = () => {
+    setIsMuted((muted: boolean) => !muted);
+  };
+
   return (
     <div className="play-video__container">
-      <video src={video["url"]} autoPlay loop />
+      <ReactPlayer url={video.url} playing={true} muted={isMuted} playsinline={true} onClick={controlMute} />
     </div>
   );
 };
