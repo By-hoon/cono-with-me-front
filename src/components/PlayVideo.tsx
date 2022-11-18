462f7f0acdff569268e690ff2b04f3c3f818d861
Yeunhun Bae <sbaru1997@naver.com>
2022-11-18 22:32:49 +0900

    [#13]feat: 동영상 진행 바 구현
    - 클릭 시 동영상 재생 위치 변경 가능

--- src/components/PlayVideo.tsx
+++ src/components/PlayVideo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import ReactPlayer from "react-player";
 
 interface VideoProps {
@@ -7,14 +7,40 @@ interface VideoProps {
 
 const PlayVideo = ({ video }: VideoProps) => {
   const [isMuted, setIsMuted] = useState(true);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const videoRef = useRef<ReactPlayer>(null);
 
   const controlMute = () => {
     setIsMuted((muted: boolean) => !muted);
   };
+  const onChangeCurrentTime = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const valueToNumber = Number(e.target.value);
+    setCurrentTime(valueToNumber);
+    videoRef.current?.seekTo(valueToNumber);
+  }, []);
 
   return (
     <div className="play-video__container">
-      <ReactPlayer url={video.url} playing={true} muted={isMuted} playsinline={true} onClick={controlMute} />
+      <ReactPlayer
+        url={video.url}
+        ref={videoRef}
+        playing={true}
+        muted={isMuted}
+        playsinline={true}
+        onProgress={(progress) => setCurrentTime(progress.playedSeconds)}
+        onClick={controlMute}
+      />
+      <div className="video-seek__container">
+        <input
+          type="range"
+          className="video-seek"
+          min={0}
+          max={videoRef.current?.getDuration()}
+          value={currentTime}
+          onChange={onChangeCurrentTime}
+        />
+      </div>
     </div>
   );
 };
