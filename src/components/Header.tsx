1650f70f4d57e62cde273adb71e1525f6367ec68
By-hoon <sbaru1997@naver.com>
2022-11-25 22:50:01 +0900

    [#15]feat: 데스크톱(browser) 헤더 기본 UI 구성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,12 +8,46 @@ interface DetailHeaderProps {
 }
 
 export const Header = () => {
+  const onSubmitSearch = () => {
+    //검색 로직
+  };
+
   return (
     <>
       {isBrowser ? (
         <div className="header__container">
           <div className="logo__container">
             <Link to="/">코노윗미</Link>
+          </div>
+          <div className="header-category__container">
+            <div className="header-category">윗미</div>
+            <div className="header-category">라이브</div>
+          </div>
+          <div className="header-search__container">
+            <form onSubmit={onSubmitSearch}>
+              <Icon icon="ic:baseline-search" />
+              <input
+                className="header-search__input"
+                type="text"
+                name="header-search"
+                placeholder="통합 검색"
+              />
+            </form>
+          </div>
+          <div className="header-tool__container">
+            <div className="profile-link__container">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/myplaylist-783c8.appspot.com/o/KakaoTalk_20210927_025101163.jpg?alt=media&token=4f3c0769-6a1b-40ee-a787-a4fd7e11487f"
+                alt="profile-link"
+              />
+            </div>
+            <div className="create-button__container">
+              <Icon icon="material-symbols:add-box-rounded" />
+              <div className="create-option__container">
+                <div className="create-link__container">윗미 생성</div>
+                <div className="create-link__container">라이브 생성</div>
+              </div>
+            </div>
           </div>
         </div>
       ) : (
