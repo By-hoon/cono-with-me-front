d29b376156ccf0cc3f88069f2cf3df482d37399b
By-hoon <sbaru1997@naver.com>
2022-11-30 17:19:41 +0900

    [#15]style: 모바일 헤더 스타일링

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -135,46 +135,48 @@ export const Header = () => {
           </div>
         </div>
       ) : (
-        <div className="m-header__container">
+        <div className="m-header__container flex">
           <div className="m-header-icon__container" onClick={onClickSidebar} ref={sidebarRef}>
             <Icon icon="material-symbols:menu-rounded" />
             {showSidebar ? (
-              <div className="sidebar__container">
-                <div className="sidebar-header__container">
-                  <div className="m-logo__container">
-                    <Link to="/">코노윗미</Link>
+              <div className="m-background">
+                <div className="sidebar__container">
+                  <div className="sidebar-header__container flex">
+                    <div className="m-sidebar-logo__container">
+                      <Link to="/">코노윗미</Link>
+                    </div>
+                    <div className="sidebar-profile-image__container">
+                      <img
+                        className="sidebar-profile__image"
+                        src="https://firebasestorage.googleapis.com/v0/b/myplaylist-783c8.appspot.com/o/KakaoTalk_20210927_025101163.jpg?alt=media&token=4f3c0769-6a1b-40ee-a787-a4fd7e11487f"
+                        alt="profile-link"
+                      />
+                    </div>
                   </div>
-                  <div className="sidebar-profile-image__container">
-                    <img
-                      className="sidebar-profile__image"
-                      src="https://firebasestorage.googleapis.com/v0/b/myplaylist-783c8.appspot.com/o/KakaoTalk_20210927_025101163.jpg?alt=media&token=4f3c0769-6a1b-40ee-a787-a4fd7e11487f"
-                      alt="profile-link"
-                    />
+                  <div className="sidebar-profile-menu__container flex">
+                    <Link to="/my" className="sidebar-profile-menu">
+                      마이페이지
+                    </Link>
+                    <div className="sidebar-profile-menu">로그아웃</div>
                   </div>
-                </div>
-                <div className="sidebar-profile-menu__container">
-                  <Link to="/my" className="sidebar-profile-menu">
-                    마이페이지
-                  </Link>
-                  <div className="sidebar-profile-menu">로그아웃</div>
-                </div>
-                <div className="sidebar-category__container">
-                  <Link
-                    to="/with"
-                    className={` ${
-                      currentLocation === "with" ? "sidebar-category--current" : "sidebar-category"
-                    }`}
-                  >
-                    윗미
-                  </Link>
-                  <Link
-                    to="/live"
-                    className={` ${
-                      currentLocation === "live" ? "sidebar-category--current" : "sidebar-category"
-                    }`}
-                  >
-                    라이브
-                  </Link>
+                  <div className="sidebar-category__container flex">
+                    <Link
+                      to="/with"
+                      className={` ${
+                        currentLocation === "with" ? "sidebar-category--current" : "sidebar-category"
+                      }`}
+                    >
+                      윗미
+                    </Link>
+                    <Link
+                      to="/live"
+                      className={` ${
+                        currentLocation === "live" ? "sidebar-category--current" : "sidebar-category"
+                      }`}
+                    >
+                      라이브
+                    </Link>
+                  </div>
                 </div>
               </div>
             ) : null}
@@ -183,25 +185,27 @@ export const Header = () => {
             <Link to="/">코노윗미</Link>
           </div>
           <div className="flex">
-            <div className="m-header-search__container">
+            <div className="m-header-search__foward">
               <div className="m-header-icon__container" onClick={appearBehindSearch}>
                 <Icon icon="ic:baseline-search" />
               </div>
               {showBehindSearch ? (
-                <div className="m-header-search__container--behind">
-                  <div className="m-header-search__container">
-                    <form onSubmit={onSubmitSearch}>
-                      <Icon icon="ic:baseline-search" />
-                      <input
-                        className="m-header-search__input"
-                        type="text"
-                        name="header-search"
-                        placeholder="통합 검색"
-                      />
-                    </form>
-                    <button className="m-header-search-cancle__button" onClick={disappearBehindSearch}>
-                      취소
-                    </button>
+                <div className="m-background">
+                  <div className="m-header-search__container--behind flex">
+                    <div className="m-header-search__container">
+                      <form onSubmit={onSubmitSearch} className="flex">
+                        <Icon icon="ic:baseline-search" />
+                        <input
+                          className="m-header-search__input"
+                          type="text"
+                          name="header-search"
+                          placeholder="통합 검색"
+                        />
+                      </form>
+                    </div>
+                    <div className="m-header-search-cancle__container flex">
+                      <span onClick={disappearBehindSearch}>취소</span>
+                    </div>
                   </div>
                 </div>
               ) : null}
@@ -209,7 +213,7 @@ export const Header = () => {
             <div className="m-header-icon__container" ref={createRef}>
               <Icon icon="material-symbols:add-box-rounded" onClick={onClickCreateMenu} />
               {showCreateMenu ? (
-                <div className="create-option__container">
+                <div className="m-create-option__container flex">
                   <Link to="/with/create" className="create-link__container">
                     윗미 생성
                   </Link>
