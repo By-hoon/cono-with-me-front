976053a330d44515896d35666425d335acfd17e5
Yeunhun Bae <sbaru1997@naver.com>
2022-12-05 19:29:10 +0900

    [#17]feat: 라이브 목록 정렬 옵션 구현
    옵션 목록은 추후 수정

--- src/components/Lives.tsx
+++ src/components/Lives.tsx
@@ -1,15 +1,56 @@
-import { useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
+import { useEffect, useRef, useState } from "react";
+import { sortOptions } from "../shared/Constants";
 import { LiveCardProps } from "../shared/Props";
 import { LivesData } from "../test/data";
 import LiveCard from "./LiveCard";
 
 const Lives = () => {
   const [lives, setLives] = useState<Array<LiveCardProps>>();
+  const [selectedSortOption, setSelectedSortOption] = useState("정렬 없음");
+  const [showSortOption, setShowSortOption] = useState(false);
+
+  const sortOptionRef = useRef<HTMLInputElement>(null);
+
+  const changeSortOption = (option: string) => {
+    setSelectedSortOption(option);
+  };
+
+  const onClickSortOption = () => {
+    setShowSortOption((show) => !show);
+  };
+  const onClickOutSide = (e: any) => {
+    if (showSortOption && sortOptionRef.current && !sortOptionRef.current.contains(e.target)) {
+      setShowSortOption(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", onClickOutSide);
+    return () => {
+      document.removeEventListener("click", onClickOutSide);
+    };
+  });
   useEffect(() => {
     setLives(LivesData);
   }, []);
   return (
     <div className="lives__container flex">
+      <div className="lives-header__container">
+        <div className="selected-sort-option flex" onClick={onClickSortOption} ref={sortOptionRef}>
+          {selectedSortOption}
+          <Icon icon="material-symbols:keyboard-arrow-down-rounded" />
+        </div>
+        {showSortOption ? (
+          <div className="sort-options__container">
+            {sortOptions.map((option) => (
+              <div key={option} className="sort-option" onClick={() => changeSortOption(option)}>
+                {option}
+              </div>
+            ))}
+          </div>
+        ) : null}
+      </div>
       {lives?.map((live) => (
         <LiveCard key={live.id} id={live.id} title={live.title} thumbnail={live.thumbnail} />
       ))}
