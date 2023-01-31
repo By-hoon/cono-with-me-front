1bf3f1d59a89ba0421b5924d4bb6a910672d4e87
Yeunhun Bae <sbaru1997@naver.com>
2023-01-31 16:22:36 +0900

    [#25]feat: 도움말 렌더링 제어
    아이콘 클릭 시 도움말을 열고 닫을 수 있도록 구현

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -1,14 +1,16 @@
 import { Icon } from "@iconify/react";
+import { useState } from "react";
 
 interface HelpProps {
   children: React.ReactNode;
 }
 
 const Help = ({ children }: HelpProps) => {
+  const [clicked, setClicked] = useState(false);
   return (
     <div className="help__container">
-      <Icon icon="material-symbols:help" />
-      <div className="help-message">{children}</div>
+      <Icon icon="material-symbols:help" onClick={() => setClicked(!clicked)} />
+      {clicked ? <div className="help-message">{children}</div> : null}
     </div>
   );
 };
