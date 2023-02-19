bd247a086bee4156f685fe7e7aeb3ce70688fb27
Yeunhun Bae <sbaru1997@naver.com>
2023-02-19 18:48:16 +0900

    [#34]feat: 윗미 신청 컴포넌트 구현

---
+++ src/components/RequestForm.tsx
@@ -0,0 +1,57 @@
+import { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import mainApi from "../apis/mainApi";
+import { SUCCESS } from "../shared/Constants";
+import Title from "./common/Title";
+
+interface RequestFormProps {
+  withId: string;
+}
+
+const RequestForm = ({ withId }: RequestFormProps) => {
+  const [comment, setComment] = useState("");
+
+  const navigate = useNavigate();
+
+  const changeComment = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  }, []);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mainApi
+      .post(`/recruitment/${withId}/requests`, {
+        comment,
+      })
+      .then((res) => {
+        alert(SUCCESS.REQUESTWITH);
+        navigate("/with");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="request-form__container">
+      <div className="request-with-header__container flex">
+        <Title title={"윗미 신청"} />
+      </div>
+      <form onSubmit={onSubmit}>
+        <div className="comment-input__container">
+          <textarea
+            className="comment__input"
+            placeholder="신청 시 함께 보낼 내용을 작성해 주세요."
+            value={comment}
+            onChange={changeComment}
+            required
+          />
+        </div>
+        <div className="submit__container">
+          <input type="submit" value="신청하기" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default RequestForm;
