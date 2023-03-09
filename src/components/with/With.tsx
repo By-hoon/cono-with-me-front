2f2dd45a1d4ca0b574e624ff438750d65b0b97de
By-hoon <sbaru1997@naver.com>
2023-03-09 16:08:51 +0900

    [#34]feat: 윗미 신청 남은 시간 표기

--- src/components/with/With.tsx
+++ src/components/with/With.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import mainApi from "../../apis/mainApi";
@@ -9,16 +10,25 @@ const With = () => {
   const withId = useLocation().state.withId as string;
   const navigate = useNavigate();
 
-  const formattingTime = (time: string) => {
-    const timeSplit = time.split("T");
-    const targetDate = timeSplit[0].split("-");
-    const targetTime = timeSplit[1].split(":").map((string) => Number(string));
-    const year = targetDate[0].split("").slice(2, 4).join("");
-    const month = targetDate[1];
-    const date = targetDate[2];
-    const hour = targetTime[0] > 12 ? `오후 ${targetTime[0] - 12}` : `오전 ${targetTime[0]}`;
-    const minute = targetTime[1];
-    return `${year}년 ${month}월 ${date}일 ${hour}시 ${minute}분`;
+  const formattingTime = (order: string, time: string) => {
+    if (order === "fullTime") {
+      const timeSplit = time.split("T");
+      const targetDate = timeSplit[0].split("-");
+      const targetTime = timeSplit[1].split(":").map((string) => Number(string));
+      const year = targetDate[0].split("").slice(2, 4).join("");
+      const month = targetDate[1];
+      const date = targetDate[2];
+      const hour = targetTime[0] > 12 ? `오후 ${targetTime[0] - 12}` : `오전 ${targetTime[0]}`;
+      const minute = targetTime[1];
+      return `${year}년 ${month}월 ${date}일 ${hour}시 ${minute}분`;
+    }
+    if (order === "remainTime") {
+      const remainTime = Math.floor(moment(time).diff(moment()) / 60000);
+      const remainHour = Math.floor(remainTime / 60);
+      const remainMinute = remainTime % 60;
+      return `${remainHour}시간 ${remainMinute}분`;
+    }
+    return null;
   };
 
   useEffect(() => {
@@ -38,7 +48,7 @@ const With = () => {
         <div className="with-title">{w?.title}</div>
       </div>
       <div className="with-time__container">
-        <div className="with-start-time">{w ? formattingTime(w.startedAt) : null}</div>
+        <div className="with-start-time">{w ? formattingTime("fullTime", w.startedAt) : null}</div>
       </div>
       <div className="with-place__container">
         <div className="with-place">{w?.place}</div>
@@ -51,6 +61,9 @@ const With = () => {
       </div>
       <div className="with-content__container">
         <div className="with-content">{w?.content}</div>
+      </div>
+      <div className="remain-time__container">
+        <div className="with-start-time">마감 {w ? formattingTime("remainTime", w.expiredAt) : null}전</div>
       </div>
       <div className="with-request__container">
         <button
