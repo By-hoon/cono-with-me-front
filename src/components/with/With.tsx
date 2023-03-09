3ef366d8d439172276b277faf7cef9570807c164
By-hoon <sbaru1997@naver.com>
2023-03-09 16:35:32 +0900

    [#34]refactor: 윗미 정보 호출중에는 로딩 화면을 출력하도록 수정

--- src/components/with/With.tsx
+++ src/components/with/With.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import mainApi from "../../apis/mainApi";
 import { WithProps } from "../../shared/Props";
+import Loading from "../common/Loading";
 
 const With = () => {
   const [w, setW] = useState<WithProps>();
@@ -44,45 +45,51 @@ const With = () => {
   }, []);
   return (
     <div className="with__container">
-      <div className="with-title__container">
-        <div className="with-title">{w?.title}</div>
-      </div>
-      <div className="with-time__container">
-        <div className="with-start-time">{w ? formattingTime("fullTime", w.startedAt) : null}</div>
-      </div>
-      <div className="with-place__container">
-        <div className="with-place">{w?.place}</div>
-      </div>
-      <div className="with-participant__container">
-        <div className="with-participant">{w?.participant}</div>
-      </div>
-      <div className="with-genre__container">
-        <div className="with-genre">{w?.genre}</div>
-      </div>
-      <div className="with-content__container">
-        <div className="with-content">{w?.content}</div>
-      </div>
-      <div className="remain-time__container">
-        <div className="with-start-time">마감 {w ? formattingTime("remainTime", w.expiredAt) : null}전</div>
-      </div>
-      <div className="with-request__container">
-        <button
-          className="with-request__button"
-          onClick={() => {
-            navigate("/with/request", { state: { withId: withId } });
-          }}
-        >
-          신청
-        </button>
-        <button
-          className="with-cancel__button"
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          취소
-        </button>
-      </div>
+      {w ? (
+        <>
+          <div className="with-title__container">
+            <div className="with-title">{w.title}</div>
+          </div>
+          <div className="with-time__container">
+            <div className="with-start-time">{formattingTime("fullTime", w.startedAt)}</div>
+          </div>
+          <div className="with-place__container">
+            <div className="with-place">{w.place}</div>
+          </div>
+          <div className="with-participant__container">
+            <div className="with-participant">{w.participant}</div>
+          </div>
+          <div className="with-genre__container">
+            <div className="with-genre">{w.genre}</div>
+          </div>
+          <div className="with-content__container">
+            <div className="with-content">{w.content}</div>
+          </div>
+          <div className="remain-time__container">
+            <div className="with-start-time">마감 {formattingTime("remainTime", w.expiredAt)}전</div>
+          </div>
+          <div className="with-request__container">
+            <button
+              className="with-request__button"
+              onClick={() => {
+                navigate("/with/request", { state: { withId: withId } });
+              }}
+            >
+              신청
+            </button>
+            <button
+              className="with-cancel__button"
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              취소
+            </button>
+          </div>
+        </>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
